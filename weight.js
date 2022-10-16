1b2767ebcd616b0329ea30c6a3fdcedffddfa53a
Jairo Ramirez <jairoramirezu@gmail.com>
2022-10-16 00:09:32 -0400

    better practices, better and shorter code

--- weight.js
+++ weight.js
@@ -4,94 +4,55 @@ let biceps = document.getElementById("inputBiceps");
 let triceps = document.getElementById("inputTriceps");
 let subescapular = document.getElementById("inputSubescapular");
 let crestaI = document.getElementById("inputCrestaI");
-let result = document.getElementById("resultG");
+let printResult = document.getElementById("resultG");
 
-let estC = function (){
-    if (sex.value==0 && age.value==0) {
-      let C = 0;
-      return C;
-    } else if (sex.value==1 && age.value==1){
-      let C = 1.1620;
-      return C;
-    } else if (sex.value==1 && age.value==2){
-      let C = 1.1631;
-      return C;
-    } else if (sex.value==1 && age.value==3){
-      let C = 1.1422;
-      return C;
-    } else if (sex.value==1 && age.value==4){
-      let C = 1.1620;
-      return C;
-    } else if (sex.value==1 && age.value==5){
-      let C = 1.1715;
-      return C;
-    }else if (sex.value==2 && age.value==1){
-      let C = 1.1549;
-      return C;
-    } else if (sex.value==2 && age.value==2){
-      let C = 1.1599;
-      return C;
-    } else if (sex.value==2 && age.value==3){
-      let C = 1.1423;
-      return C;
-    } else if (sex.value==2 && age.value==4){
-      let C = 1.1333;
-      return C;
-    } else if (sex.value==2 && age.value==5){
-      let C = 1.1339;
-      return C;
-    } else {
-      let C = 0;
-      return C;
-    }
-  }
-
-
-let estM = function () {
+const setVal = () => {
   if (sex.value==0 && age.value==0) {
-    let M = 0;
-    return M;
+    let Val = [0, 0];
+    return Val;
   } else if (sex.value==1 && age.value==1){
-    let M = 0.0630;
-    return M;
+    let Val = [1.1620, 0.0630];
+    return Val;
   } else if (sex.value==1 && age.value==2){
-    let M = 0.0632;
-    return M;
+    let Val = [1.1631, 0.0632];
+    return Val;
   } else if (sex.value==1 && age.value==3){
-    let M = 0.0544;
-    return M;
+    let Val = [1.1422, 0.0544];
+    return Val;
   } else if (sex.value==1 && age.value==4){
-    let M = 0.0700;
-    return M;
+    let Val = [1.1620, 0.0700];
+    return Val;
   } else if (sex.value==1 && age.value==5){
-    let M = 0.0779;
-    return M;
+    let Val = [1.1715, 0.0779];
+    return Val;
   }else if (sex.value==2 && age.value==1){
-    let M = 0.0678;
-    return M;
+    let Val = [1.1549, 0.0678];
+    return Val;
   } else if (sex.value==2 && age.value==2){
-    let M = 0.0717;
-    return M;
+    let Val = [1.1599, 0.0717];
+    return Val;
   } else if (sex.value==2 && age.value==3){
-    let M = 0.0632;
-    return M;
+    let Val = [1.1423, 0.0632];
+    return Val;
   } else if (sex.value==2 && age.value==4){
-    let M = 0.0612;
-    return M;
+    let Val = [1.1333, 0.0612];
+    return Val;
   } else if (sex.value==2 && age.value==5){
-    let M = 0.0645;
-    return M;
+    let Val = [1.1339, 0.0645];
+    return Val;
   } else {
-    let M = 0;
-    return M;
+    let Val = [0, 0];
+    return Val;
+  }
 }
-}
+
+const estC = () => parseFloat(setVal()[0]);
+const estM = () => parseFloat(setVal()[1]);
 
 function calcPliegues (biceps, triceps, subescapular, crestaI) {
   let pliegues = biceps + triceps + subescapular + crestaI;
   return pliegues;
 }
-
 
 function calcDensidad (constC, constM, sumaPliegues) {
   let densidad = constC - (constM * (Math.log10(sumaPliegues)));
@@ -103,49 +64,29 @@ function calcGrasa(densidad) {
   return grasa;
 }
 
-function manFinalSuccess() {
-    manRes();
-}
-
-function manFinalFail() {
-  resultG.innerText = "Debes llenar todos los campos ⚠️";
-}
-
-
-let manRes = function () {
-    let manC = estC();
-    let manM = estM();
-    let manFolds = calcPliegues(biceps.valueAsNumber, triceps.valueAsNumber, subescapular.valueAsNumber, crestaI.valueAsNumber);
-    let manDen = calcDensidad(manC, manM, manFolds);
-    let manGras = calcGrasa(manDen);
-    let resultado = manGras.toFixed(2);
-    if (sex.value==2 && resultado<=14) {
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a grasa esencial 🤯";
-    } else if (sex.value==2 && resultado>14 && resultado<=22){
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a un atleta 💪";
-    } else if (sex.value==2 && resultado>22 && resultado<=25) {
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a una buena aptitup física 🤩";
-    } else if (sex.value==2 && resultado>25 && resultado<=31) {
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a un riesgo potencial 🙁";
-    } else if (sex.value==2 && resultado>32) {
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a una persona obesa 😤";
-    }else if (sex.value==1 && resultado<=5){
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a grasa esencial 🤯";
-    } else if (sex.value==1 && resultado>5 && resultado<=13) {
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a un atleta 💪";
-    } else if (sex.value==1 && resultado>13 && resultado<=18) {
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a una buena aptitup física 🤩";
-    } else if (sex.value==1 && resultado>18 && resultado<=24) {
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a un riesgo potencial 🙁";
-    } else if (sex.value==1 && resultado>24) {
-      resultG.innerText = "Tienes un porcentaje de grasa de " + resultado + "% correspondiente a una persona obesa 😤";
+let finalResult = () => {
+    let folds = calcPliegues(biceps.valueAsNumber, triceps.valueAsNumber, subescapular.valueAsNumber, crestaI.valueAsNumber);
+    let density = calcDensidad(estC(), estM(), folds);
+    let grass = calcGrasa(density);
+    let result = grass.toFixed(2);
+    const text = "Tienes un porcentaje de grasa de ";
+    if (sex.value==2 && result<=14 || sex.value==1 && result<=5) {
+      printResult.innerText = text + result + "% correspondiente a grasa esencial 🤯";
+    } else if (sex.value==2 && result>14 && result<=22 || sex.value==1 && result>5 && result<=13){
+      printResult.innerText = text + result + "% correspondiente a un atleta 💪";
+    } else if (sex.value==2 && result>22 && result<=25 || sex.value==1 && result>13 && result<=18) {
+      printResult.innerText = text + result + "% correspondiente a una buena aptitup física 🤩";
+    } else if (sex.value==2 && result>25 && result<=31 || sex.value==1 && result>18 && result<=24) {
+      printResult.innerText = text + result + "% correspondiente a un riesgo potencial 🙁";
+    } else if (sex.value==2 && result>32 || sex.value==1 && result>24) {
+      printResult.innerText = text + result + "% correspondiente a una persona obesa 😤";
     }
 }
 
-function manFinal() {
-  if (sex.value>0 && age.value>0 && biceps.valueAsNumber>0 && triceps.valueAsNumber>0 && subescapular.valueAsNumber>0 && crestaI.valueAsNumber>0) {
-    manFinalSuccess();
+function calc() {
+  if (sex.value>0 && age.value>0 && biceps.value>0 && triceps.value>0 && subescapular.value>0 && crestaI.value>0) {
+    finalResult();
   } else {
-    manFinalFail();
+      result.innerText = "Debes llenar todos los campos ⚠️";
   }
 }
